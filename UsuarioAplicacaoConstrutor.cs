5ff224a9155a506a755ac9d93c4e0b5ad8160488
rodriguesflavio1 <rodriguesflavio1@gmail.com>
2017-02-07 23:39:43 -0200

    Add files via upload

---
+++ UsuarioAplicacaoConstrutor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.RepositorioADO;
+
+namespace Biblioteca.Aplicacao
+{
+    public class UsuarioAplicacaoConstrutor
+    {
+        // método encapsula o método UsuarioRepositorioADO e passa ela como parametro
+        public static UsuarioAplicacao UsuarioAplicacaoADO()
+        {
+            return new UsuarioAplicacao(new UsuarioRepositorioADO());
+        }
+
+        public static UsuarioAplicacao UsuarioAplicacaoUsuarioADO()
+        {
+            return new UsuarioAplicacao(new UsuarioRepositorioLoginADO());
+        }
+
+        public static UsuarioAplicacao UsuarioAplicacaoProdutoRelatorioADO()
+        {
+            return new UsuarioAplicacao(new ProdutoRepositorioRelatorioADO());
+        }
+       
+    }
+}
