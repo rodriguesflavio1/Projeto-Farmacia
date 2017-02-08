a0badb2bc668b0729214b3d97c2df922931207a9
rodriguesflavio1 <rodriguesflavio1@gmail.com>
2017-02-07 23:41:48 -0200

    Add files via upload

---
+++ ProdutoRelatorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Biblioteca.Dominio
+{
+    public class ProdutoRelatorio
+    {
+        public int produtoId { get; set; }
+        
+        [DisplayName("Nome do Medicamento")]
+        public string nome { get; set; }
+       
+        [DisplayName("Principio Ativo")]
+        public string principioAtivo { get; set; }
+
+        [DisplayFormat(DataFormatString = "mm/yyyy")]
+        [DisplayName("Data de Validade")]
+        public DateTime dataRetirada { get; set; }
+
+        [DisplayName("Quantidade")]
+        public int quantidade { get; set; }
+
+
+
+       
+
+
+    }
+}
